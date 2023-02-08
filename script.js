f9e1c2c8972107eb21bbd129d1ee37a5ef75f69b
unknown <shamiullipu@gmail.com>
2023-02-08 17:42:09 +0600

    added js file

---
+++ script.js
@@ -0,0 +1,9 @@
+/* 
+
+ * Title: To Do List JS file
+ * Description: This file has all the DOM js associated with the To Do App project
+ * Author: Shamiul learning with Sumit 
+ * Date: 02/08/2023
+ *
+
+*/
